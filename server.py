9d110fd0d3da743b4d872a826404b258ebccfcc2
bree parker <breannaparker1991@gmail.com>
2022-11-08 13:29:36 -0800

    finished cash register code

--- server.py
+++ server.py
@@ -5,44 +5,43 @@
 #11/7/22
 
 
-# Your program should have two getter methods.
-# getTotal – returns totalPrice
-# getCount – returns the itemCount of the cart
-# Your program must create an instance of the CashRegister class within your main function.
-# Your program should have a loop in main which allows the user to continue to add items to the cart until they request to quit.
-# The output should be formatted as currency. Be sure to investigate the locale class. You will need to call locale.setlocale and locale.currency.
-
-
+import locale
 
 class CashRegister:
   def __init__(self):
     self.items = 0
-    self.price = 0.0
+    self.price = float(0.00)
+  
     
-  def addItem(self,price,items): #keeps track of total number of items in cart
-    self.price = price
-    items = 0
-    items = items + 1
+  def addItems(self,price): #keeps track of total number of items in cart
+    self.price += price
+    self.items += 1)
     
-  def getTotal():  #returns total price
+  def getTotal(self):  #returns total price
     return self.price
   
-  def getCount():  #return the item count of the cart
+  def getCount(self):  #return the item count of the cart
     return self.items
   
+  def clearCart(self):  #clears cart for another user or checkout
+    self.items = 0
+    self.price = float(0.00)
 
-def main():
-  user_name = input('What is your name?\n')
+def main(): 
+  user_name = input('What is your name?\n') #weclomes user
   print("Hello",user_name)
-  user_name = CashRegister()
+  locale.setlocale(locale.LC_ALL, 'en_US')
+  user_name = CashRegister()  #user is using the cash register
   while True:
-    line = input ("Would you like to add another food item to your cart? Choose y or n \n")
-    if line  == "y":
-      price = input("please input the price of the item\n")
-      user_name.addItem(price)
-    elif line == "n":
-      print(user_name.getTotal())
-      print(user_name.getCount())
+    line = input ("Would you like to add another food item to your cart? Choose y or n \n") 
+    if line.lower()  == "y":
+      price = float(input("please input the price of the item\n"))
+      user_name.addItems(price)   #user adds prices to cart
+    elif line.lower() == "n":
+      print("Your total checkout price:", locale.currency(user_name.getTotal()) )
+      # int(float(locale.currency(user_name.getTotal())))
+      print("Your total item count", user_name.getCount())
+      user_name.clearCart() #clears cart for another user/checkout
       break
     else:
         print("Error")
